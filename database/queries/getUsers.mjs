0385ba07588f569104af5a80d311f91b61c5e076
Younnes Chebli <younnesc@gmail.com>
2022-12-01 16:08:24 +0100

    updated get users

--- database/queries/getUsers.mjs
+++ database/queries/getUsers.mjs
@@ -3,7 +3,7 @@ import client from "../clients.mjs";
 const getUsers = async () => {
     try {
         const res = await client.query("SELECT * FROM users");
-        console.log(res.rows);
+        return res.rows;
     } catch(err) {
         console.log("Failed to select data", err.stack);
     }
