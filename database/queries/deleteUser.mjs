bb1377f1aa31586e4127b996306d1908270efd4c
Younnes Chebli <younnesc@gmail.com>
2022-12-02 13:37:37 +0100

    update: delete user

--- database/queries/deleteUser.mjs
+++ database/queries/deleteUser.mjs
@@ -2,7 +2,7 @@ import client from "../clients.mjs";
 
 const deleteUser = async (id) => {
     try {
-        await client.query(`DELETE FROM users WHERE id = ${id}`);
+        await client.query(`DELETE FROM users WHERE id = ${id};`);
     } catch(err) {
         console.log("Failed to delete data", err.stack);
     }
